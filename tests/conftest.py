a3114c3bb4e8ee909606f6fe9754ff7c0334bc9d
Samuel Mbote <mbotesamuel9@gmail.com>
2026-07-17 19:03:27 -0700

    Keep verification output clean

--- tests/conftest.py
+++
@@ -1,7 +0,0 @@
-"""Test-suite configuration for known third-party compatibility notices."""
-
-import warnings
-
-from starlette._utils import StarletteDeprecationWarning
-
-warnings.filterwarnings("ignore", category=StarletteDeprecationWarning)
